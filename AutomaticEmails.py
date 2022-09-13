7f0aec3680be7137bf0d17eb15d06ad9c189ccc8
Cole Hansen <99274668+ProfessionalGooseChaser@users.noreply.github.com>
2022-09-13 15:21:30 -0400

    AutomaticPython.py v1.01
    Adds the yesterday function and find_file function. These will find the file for the day that I would like to send.

--- AutomaticEmails.py
+++ AutomaticEmails.py
@@ -1,14 +1,16 @@
 #Sending automatic email
-from email import *
+#from email import *
 #tried pip installing this class, can't access the subprocesses, ask for help
 import requests
 from bs4 import BeautifulSoup
 import json
 import smtplib
 import os
+from datetime import date
+from datetime import timedelta
 
 #nba api
-URL = "http://data.nba.net/10s/prod/v1/today.json"
+URL = "http://data.nba.net/10s/prod/v1"
 #https://www.quora.com/Is-there-an-NBA-API-for-free-that-has-live-stats
 def ReadUrl(url):
     #finds the website from the url
@@ -20,23 +22,35 @@ def ReadUrl(url):
 
     return soup
 
-
+#finds and formats yesterday's date so that I can find the correct directory
+def yesterday():
+    yesterday = date.today() - timedelta(days=1)
+    yyear = str(yesterday.year)
+    ymonth = str(yesterday.month)
+    if(len(ymonth)== 1):
+        ymonth = "0" + ymonth
+    yday = str(yesterday.day)
+    return yyear + ymonth + yday
 
 #initiaing the email connection and logging in
-s = smtplib.SMTP('smtp.gmail.com', 587)
-s.ehlo()
-s.starttls()
-s.login('nbascores.py@gmail.com', 'rZwg2vAc')
+#s = smtplib.SMTP('smtp.gmail.com', 587)
+#s.ehlo()
+#s.starttls()
+#s.login('nbascores.py@gmail.com', 'rZwg2vAc')
 
 #pulling the data from the api
 #https://stackoverflow.com/questions/73028029/how-to-get-stats-games-of-a-specific-date-using-data-nba-net
 #today's Scoreboard
-file = "/prod/v1/20220912/scoreboard.json"
+FILE = "/20220410/scoreboard.json"
+
+#finds the correct json extension
+def find_file():
+    return "/" + yesterday() + "/scoreboard.json"
 
 def get_links(url, file):
     data = requests.get(url + file).json()
-    links = data['links']
-    return links
+    #links = data['links']
+    return data
 
 def get_scores(url, links):
     scoreboard = links['currentScoreboard']
@@ -49,14 +63,12 @@ def get_scores(url, links):
         away = g['vTeam']
         ascore = g[''] #find the link!!
         if(hscore > ascore):
-            temp = home + ": " + hscore + "beat " + away + ": " + ascore"
+            temp = home + ": " + hscore + "beat " + away + ": " + ascore
         else:
-            temp = away + ": " + ascore + "beat " + home + ": " + hscore"
+            temp = away + ": " + ascore + "beat " + home + ": " + hscore
         scores.append(temp)
-     return scores
+    return scores
 
 
-            
-    
-    
+
     
