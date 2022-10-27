33dcdf6f7927097fb7a8cae7b02ab8f981384270
Cole Hansen <99274668+ProfessionalGooseChaser@users.noreply.github.com>
2022-10-27 13:15:31 -0400

    Backlight.py V1.02
    So I changed a lot, I got rid of the class objects. I also used threads instead of processes. My next problem is accessing the values.

--- Backlight.py
+++ Backlight.py
@@ -1,52 +1,63 @@
 from PIL import ImageGrab
 import time
-from multiprocessing import Process
+from threading import *
 
 start = time.time()
 
-class light():
-    def __init__(self, minX, minY, rangeX, rangeY):
-        self.rgb = ()
-        self.minX = minX
-        self.rangeX = rangeX +1
-        self.minY = minY
-        self.rangeY = rangeY +1
-
-    def __str__(self) -> str:
-        return str(self.rgb)
-
-    def average(self):
-        sqr = ImageGrab.grab(bbox = (self.minX, self.minY, self.minX + self.rangeX, self.minY + self.rangeY))
-        px = sqr.load()
-        totalRGB = (0, 0, 0)
-        for x in range(self.rangeX):
-            for y in range(self.rangeY):
-                totalRGB = tuple(map(sum, zip(px[x,y], totalRGB))) #Stop calling this 225 times. Call it once
-                print(x, y)
-        self.rgb = tuple(ti/(self.rangeX * self.rangeY) for ti in totalRGB)
-
 SCREEN = (1440, 900)
-PERIMETER = 1440 * 2 + 900 * 2
-lights = []
+_LIGHTS = []
 
+def average2(minX, minY, rangeX, rangeY, LIGHTS):
+    sqr = ImageGrab.grab(bbox = (minX, minY, minX + rangeX, minY + rangeY))
+    px = sqr.load()
+    totalRGB = (0, 0, 0)
+    for x in range(rangeX):
+        for y in range(rangeY):
+            totalRGB = tuple(map(sum, zip(px[x,y], totalRGB))) 
+    rgb = tuple(ti/(rangeX * rangeY) for ti in totalRGB)
+    LIGHTS.append(rgb)
+
+lights = []
 for i in range(16): #90pixels for range 
-    temp = light(i*90, 0, 90, 25) #top row
+    temp = (i*90, 0, 90, 25) #top row
     lights.append(temp)
-    temp2 = light(i*90, 1415, 90, 25) #bottom row
+    temp2 = (i*90, 1415, 90, 25) #bottom row
     lights.append(temp2)
 
 for j in range(10):
-    temp = light(0, j*90, 25, 90) #Left side
+    temp = (0, j*90, 25, 90) #Left side
     lights.append(temp)
-    temp2 = light(875, j*90, 25, 90)
+    temp2 = (875, j*90, 25, 90)
     lights.append(temp2) #right side
+print("ISTG THIS SHOULD ONLY BE SEEN ONCE YOU MOTHERFUCKER")
 
-#Using processes to optimize
-#creating a process for each light object
-processes = []
+start2 = time.time()
 
+
+#Creates threads for each light, class functions suck so I did this and multithreaded it instead
+processes = [Thread(target=average2, args=(n[0], n[1], n[2], n[3], _LIGHTS)) for n in lights]
+[t1.start() for t1 in processes]
+
+#just needed to see some time calues
 end = time.time()
-print(str(end-start))
+print(str(start2-start))
+print(str(end-start2)) #takes regularly 10 seconds to boot
+
+#10 iterations
+count = 0
+while(count < 10):
+    count += 1
+    start3 = time.time()
+    _LIGHTS = []
+    if __name__ == '__main__':
+        [t2.join() for t2 in processes]
+    #send values to board
+    print(time.time() - start3) # this time is always within .001 seconds
+    
+
+
+
+
 
 #1mil - 48.25s
 #100k - 5.17s
@@ -55,3 +66,6 @@ print(str(end-start))
 #100 - 0.4s
 
 #some notes, I'm not too worried about time rn. I do want to eventually use parallelt processing to update all of the lights at onces
+
+#More notes, I optimized with multithreading.
+#I'm now doing around 100k in under 0.001 Seconds, sometimes significantly less
